495aa5cf97a307a7097ba590859ae15698cc7a29
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-08 11:39:38 +0530

    Set up the first level email news blast

---
+++ email_writing.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+
+"""
+Before running the script, make sure you've set up 2 - Step Authentication from the account settings 
+and then copy the app password using password manager in Google, if you're using Google
+"""
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = 'ragkan1499@gmail.com'
+    password = 'quhf cizr znec lerb'
+    receiver = 'ragkan1499@gmail.com'
+    
+    context = ssl.create_default_context()
+   
+    
+    with smtplib.SMTP_SSL(host, port, context= context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    
+if __name__ == '__main__':
+    send_email('This is a test message')
+    
